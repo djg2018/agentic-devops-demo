27eabe5b92b9f7224f729d4752e2cb120e39e31d
djg2018 <dheerajgundavaram@gmail.com>
2025-06-01 17:58:28 +0000

    trigger redeploy via workflow

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Agentic AI World on Google Cloud!');
   res.send('If you are seeing this message, then the deployment is successful');
 });
 
